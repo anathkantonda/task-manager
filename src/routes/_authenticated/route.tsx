658ae49ca06ce2404e9f7924b7f4adf502dfa9d5
Anath Kantonda <anathkantonda@gmail.com>
2026-01-09 10:15:35 +0200

    added greeting next to sign up page so that it's more apparent on other page like tasks page

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -41,6 +41,7 @@ function AuthLayout() {
                         </Link>
                     </div>
                     <div className="flex gap-4 items-center">
+                        <span>Hi {session?.user.name}!</span>
                         <button onClick={handleSignOut} className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition">
                             Sign Out
                         </button>
